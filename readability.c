4a2e5a0c175b87ae2e41306800147a39ab1ad4f9
TomasNagy <tomi.nagy@outlook.com>
2023-05-15 18:30:58 +0200

    Add comments

--- readability.c
+++ readability.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+// math.h included to use the round() function
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,7 +12,22 @@ int main(void)
 	string user_Text = get_string("Text: ");
 
 	// Validate text and print grade
-	printf("Grade: %i\n", validate_text(user_Text));
+	int grade = validate_text(user_Text);
+
+	if (grade < 1)
+	{
+		printf("Before grade 1\n");	
+	}
+	else if (grade > 16)
+	{
+		printf("Grade: 16+\n");	
+	}
+	else
+	{
+		printf("Grade: %i\n", grade);
+	}	
+	
+	return 0;
 }
 
 int validate_text(string text)
@@ -19,33 +35,42 @@ int validate_text(string text)
 	int letters = 0, words = 0, sentences = 0;
 
 	int text_length = strlen(text);
-
+	
+	// Iterate through the given text
 	for (int i = 0; i < text_length; i++)
 	{
+		// If character is between 'a' and 'z' or 'A' and 'Z', it is a letter
 		if ((text[i] >= 'a' && text[i] <= 'z') ||
 			(text[i] >= 'A' && text[i] <= 'Z'))
 		{
 			letters++;
 		}
+		// If character is space ' ' then, there was a word
 		else if (text[i] == ' ')
 		{
 			words++;
 		}
+		// If character is '.' or '!' or '?', there was a sentence
 		else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
 		{
 			sentences++;
 		}
 	}
-
+ 
+	// The last word is not counted because there is no space character after it
+	// so we need to add one to the word variable to count it
 	words++;
 
-	printf("letters: %i, words: %i, sentences: %i\n", letters, words, sentences);
+	// For debugging the code above
+	// printf("letters: %i, words: %i, sentences: %i\n", letters, words, sentences);
 
 	float avg_L = letters / (float) words * 100.0;
 
 	float avg_S = sentences / (float) words * 100.0; 
 
+	// Calculate the Coleman-Liau index
 	float Coleman_Liau_formula = (0.0588 * avg_L) - (0.296 * avg_S) - 15.8;
 
+	// Round the index to the nearest integer value and return it
 	return round(Coleman_Liau_formula);
 }
